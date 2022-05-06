2bf0ff27dd6860ec7b31204c3f47c871a2e688bb
David Novella Saldana <dnovella@Novella-Inspiron.localdomain>
2022-05-06 12:26:45 -0500

    Se agrega eslint.

--- app/lib/services/ExplorerService.js
+++ app/lib/services/ExplorerService.js
@@ -1,19 +1,19 @@
 class ExplorerService{
-  static filterByMission(explorers, mission){
-    const filteredByMission = explorers.filter((explorer)=>(explorer.mission == mission))
-    return filteredByMission;
-  }
+    static filterByMission(explorers, mission){
+        const filteredByMission = explorers.filter((explorer)=>(explorer.mission == mission));
+        return filteredByMission;
+    }
 
-  static getAmountOfExplorersByMission(explorers, mission){
-    const filteredByMission = explorers.filter((explorer)=>(explorer.mission == mission))
-    return filteredByMission.length;    
-  }
+    static getAmountOfExplorersByMission(explorers, mission){
+        const filteredByMission = explorers.filter((explorer)=>(explorer.mission == mission));
+        return filteredByMission.length;    
+    }
 
-  static getExplorersUserNamesByMission(explorers, mission){
-    const filteredByMission = explorers.filter((explorer)=>(explorer.mission == mission))
-    const explorersNames = filteredByMission.map((explorer)=>(explorer.name))
-    return explorersNames;
-  }
+    static getExplorersUserNamesByMission(explorers, mission){
+        const filteredByMission = explorers.filter((explorer)=>(explorer.mission == mission));
+        const explorersNames = filteredByMission.map((explorer)=>(explorer.name));
+        return explorersNames;
+    }
 }
 
 module.exports = ExplorerService;
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,31 +1,31 @@
 
-const FizzbuzzService = require('../../app/lib/services/FizzbuzzService');
+const FizzbuzzService = require("../../app/lib/services/FizzbuzzService");
 
 describe("Test for FizzBuzz Service", ()=>{
-  test("1. Test Explorer1", ()=>{       
+    test("1. Test Explorer1", ()=>{       
 
-    const explorer1 = {name: "Explorer1", score: 1}
-    FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
-    expect(explorer1.trick).toBe(1);                
-  })
+        const explorer1 = {name: "Explorer1", score: 1};
+        FizzbuzzService.applyValidationInExplorer(explorer1); // {name: "Explorer1", score: 1, trick: 1} 
+        expect(explorer1.trick).toBe(1);                
+    });
 
-  test("2. Test Explorer3", ()=>{
-    const explorer3 = {name: "Explorer3", score: 3}
-    FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
-    expect(explorer3.trick).toBe('FIZZ');                      
-  })
+    test("2. Test Explorer3", ()=>{
+        const explorer3 = {name: "Explorer3", score: 3};
+        FizzbuzzService.applyValidationInExplorer(explorer3); // {name: "Explorer3", score: 3, trick: "FIZZ"}
+        expect(explorer3.trick).toBe("FIZZ");                      
+    });
 
-  test("3. Test Explorer5", ()=>{
-    const explorer5 = {name: "Explorer5", score: 5}
-    FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
-    expect(explorer5.trick).toBe('BUZZ');                      
-  })
+    test("3. Test Explorer5", ()=>{
+        const explorer5 = {name: "Explorer5", score: 5};
+        FizzbuzzService.applyValidationInExplorer(explorer5); // {name: "Explorer5", score: 5, trick: "BUZZ"}
+        expect(explorer5.trick).toBe("BUZZ");                      
+    });
 
-  test("4. Test Explorer15", ()=>{
-    const explorer15 = {name: "Explorer15", score: 15}
-    FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
-    expect(explorer15.trick).toBe('FIZZBUZZ');                      
-  })
-})
+    test("4. Test Explorer15", ()=>{
+        const explorer15 = {name: "Explorer15", score: 15};
+        FizzbuzzService.applyValidationInExplorer(explorer15); // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+        expect(explorer15.trick).toBe("FIZZBUZZ");                      
+    });
+});
 
 
